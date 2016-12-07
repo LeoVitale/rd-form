e05e6b505242afb18a9d2fef157c5c82774d96d7
Leonardo Vitale <leonardo_vitale@hotmail.com>
2016-12-07 14:49:57 -0200

    Created Initial Config files

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+
+gulp.task('serve', ['sass'], function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch("src/sass/*.scss", ['sass']);
+    gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+gulp.task('sass', function() {
+    return gulp.src("src/sass/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("src/styles/"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
